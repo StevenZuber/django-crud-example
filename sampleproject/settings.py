aa8fdcdb84b941aee74c35256b86a86fa66be603
StevenZuber <stevenjzuber@gmail.com>
2019-03-18 17:35:44 -0600

    update configs for heroku deployment

--- sampleproject/settings.py
+++ sampleproject/settings.py
@@ -9,7 +9,6 @@ https://docs.djangoproject.com/en/2.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.1/ref/settings/
 """
-
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -121,3 +120,6 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+import django_heroku
+django_heroku.settings(locals())
